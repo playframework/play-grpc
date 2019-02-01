afae4e01bd0d19f680547f1edc180137d153cb1f
Dale Wijnand <dale.wijnand@gmail.com>
2019-02-01 11:25:28 +0000

    Update to sbt-header 5.1.0
    And cleanup CopyrightHeader

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -5,13 +5,15 @@
 package build.play.grpc
 
 import sbt._, Keys._
-import de.heikoseeberger.sbtheader.{ CommentCreator, HeaderPlugin }
+import de.heikoseeberger.sbtheader.{ CommentCreator, CommentStyle, HeaderPlugin }
 
 object CopyrightHeader extends AutoPlugin {
   import HeaderPlugin.autoImport._
 
   override def requires = HeaderPlugin
   override def trigger = allRequirements
+
+  override def buildSettings = Seq(headerEmptyLine := false)
 
   override def projectSettings = Def.settings(
     Seq(Compile, Test).flatMap { config =>
@@ -30,33 +32,34 @@ object CopyrightHeader extends AutoPlugin {
   )
 
   val CurrentYear = java.time.Year.now.getValue.toString
-  val CopyrightPattern = "Copyright \\([Cc]\\) (\\d{4}(-\\d{4})?) (Lightbend|Typesafe) Inc. <.*>".r
+  val CopyrightPattern =
+    "Copyright \\([Cc]\\) (\\d{4}(-\\d{4})?) (?:Lightbend|Typesafe) Inc. <.*>".r
   val CopyrightHeaderPattern = s"(?s).*${CopyrightPattern}.*".r
 
-  def headerFor(year: String): String =
-    s"Copyright (C) $year Lightbend Inc. <https://www.lightbend.com>"
+  def headerFor(yearRange: String): String =
+    s"Copyright (C) $yearRange Lightbend Inc. <https://www.lightbend.com>"
 
-  private def lightbendCommentCreator(commentCreator: CommentCreator) = new CommentCreator() {
-
-    def updateLightbendHeader(header: String): String = header match {
-      case CopyrightHeaderPattern(years, null, _) =>
-        if (years != CurrentYear)
-          CopyrightPattern.replaceFirstIn(header, headerFor(years + "-" + CurrentYear))
-        else
-          CopyrightPattern.replaceFirstIn(header, headerFor(years))
-      case CopyrightHeaderPattern(years, endYears, _) =>
-        CopyrightPattern.replaceFirstIn(header, headerFor(years.replace(endYears, "-" + CurrentYear)))
-      case _ =>
-        header
+  /** Updates the year range in the Lightbend copyright line */
+  private class LightbendCommentCreator(commentCreator: CommentCreator) extends CommentCreator {
+    private def updatedYearRange(header: String): Option[String] = PartialFunction.condOpt(header) {
+      case CopyrightHeaderPattern(CurrentYear, null) => CurrentYear
+      case CopyrightHeaderPattern(years, null)       => years + "-" + CurrentYear
+      case CopyrightHeaderPattern(years, endYears)   => years.replace(endYears, "-" + CurrentYear)
     }
 
     override def apply(text: String, existingText: Option[String]): String = {
+      val newText = commentCreator(text, existingText)
       existingText
-        .map(updateLightbendHeader)
-        .getOrElse(commentCreator(text, existingText))
+        .flatMap(updatedYearRange)
+        .map(yearRange => CopyrightPattern.replaceFirstIn(newText, headerFor(yearRange)))
+        .getOrElse(newText)
         .trim
     }
   }
-  val cStyleComment = HeaderCommentStyle.cStyleBlockComment.copy(commentCreator = lightbendCommentCreator(HeaderCommentStyle.cStyleBlockComment.commentCreator))
-  val twirlStyleBlockComment = HeaderCommentStyle.twirlStyleBlockComment.copy(commentCreator = lightbendCommentCreator(HeaderCommentStyle.twirlStyleBlockComment.commentCreator))
+
+  private def updateCommentCreator(style: CommentStyle) =
+    style.copy(commentCreator = new LightbendCommentCreator(style.commentCreator))
+
+  val cStyleComment          = updateCommentCreator(HeaderCommentStyle.cStyleBlockComment)
+  val twirlStyleBlockComment = updateCommentCreator(HeaderCommentStyle.twirlStyleBlockComment)
 }
