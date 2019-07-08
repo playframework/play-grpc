383250ad489922dc3286cf706872e8cd271e285a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-07-08 18:49:29 +0200

    Update grpc-stub to 1.22.0 (#121)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val play  = "2.7.3"
     val lagom = "1.5.1"
 
-    val grpc = "1.20.0" // needs to be in sync with akkaGrpc version?
+    val grpc = "1.22.0" // needs to be in sync with akkaGrpc version?
 
     val scalaTest         = "3.0.8"
     val scalaTestPlusPlay = "4.0.3"
