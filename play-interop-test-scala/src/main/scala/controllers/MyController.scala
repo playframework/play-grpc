97f65bb7581b9fd1082607fb97452fb9c4bae023
Renato Cavalcanti <renato@cavalcanti.be>
2019-10-14 15:42:36 +0200

    formatting

--- play-interop-test-scala/src/main/scala/controllers/MyController.scala
+++ play-interop-test-scala/src/main/scala/controllers/MyController.scala
@@ -14,7 +14,7 @@ import play.api.mvc.ControllerComponents
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class MyController @Inject()(
+class MyController @Inject() (
     implicit greeterClient: GreeterServiceClient,
     cc: ControllerComponents,
     exec: ExecutionContext,
