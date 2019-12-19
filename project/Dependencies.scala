a2b4ac5408f212e7af880bafaac4f7d6e4427bb0
Scala Steward <me@scala-steward.org>
2019-12-19 04:38:08 +0100

    Update lagom-javadsl-testkit, ... to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val akkaGrpc = "0.7.2" // TODO: obtain via sbt-akka-grpc?
 
     val play  = "2.7.4"
-    val lagom = "1.5.5"
+    val lagom = "1.6.0"
 
     val grpc = "1.25.0" // needs to be in sync with akkaGrpc version?
 
