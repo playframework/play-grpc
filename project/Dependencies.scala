8d664bdb8e5bd7ff804f7dffda515d99252adbc6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-20 09:58:13 +0100

    Update akka-discovery, akka-stream to 2.6.4 (#223)
    * Update akka-discovery, akka-stream to 2.6.4
    
    * Fixate more akka versions
    
    Co-authored-by: Arnout Engelen <arnout@bzzt.net>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import sbt.Keys._
 object Dependencies {
 
   object Versions {
-    val akka = "2.6.3"
+    val akka = "2.6.4"
 
     val akkaGrpc = "0.7.3" // TODO: obtain via sbt-akka-grpc?
 
@@ -24,6 +24,7 @@ object Dependencies {
   object Compile {
     val grpcStub = "io.grpc" % "grpc-stub" % Versions.grpc
 
+    val akkaActorTyped           = "com.typesafe.akka" %% "akka-actor-typed"            % Versions.akka
     val akkaClusterShardingTyped = "com.typesafe.akka" %% "akka-cluster-sharding-typed" % Versions.akka
     val akkaStream               = "com.typesafe.akka" %% "akka-stream"                 % Versions.akka
     val akkaDiscovery            = "com.typesafe.akka" %% "akka-discovery"              % Versions.akka
