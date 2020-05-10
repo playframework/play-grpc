b96088e8fb175549ffd962c4ee6b6e6cde5b9989
Scala Steward <me@scala-steward.org>
2020-05-10 16:26:40 +0200

    Update scalatest to 3.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val play  = "2.8.1"
     val lagom = "1.6.2"
 
-    val scalaTest         = "3.1.1"
+    val scalaTest         = "3.1.2"
     val scalaTestPlusPlay = "5.1.0"
 
     val macwire = "2.3.4"
