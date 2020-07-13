9f089dd744743a81f93149e12b13f69b585e5e1e
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-07-13 19:25:09 +0200

    rollback the akka-grpc dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,8 +13,8 @@ object Dependencies {
     val akkaHttp = "10.1.12"
 
     // This version must be in sync with the version of "sbt-akka-grpc" in "project/plugins.sbt"
-    val akkaGrpc = "1.0.0-32-e0342ca3-20200713-1619" // TODO: obtain via sbt-akka-grpc?
-    val grpc     = "1.30.0"                          // needs to be in sync with akkaGrpc version?
+    val akkaGrpc = "1.0.0"  // TODO: obtain via sbt-akka-grpc?
+    val grpc     = "1.30.0" // needs to be in sync with akkaGrpc version?
 
     val play  = "2.8.2"
     val lagom = "1.6.2"
