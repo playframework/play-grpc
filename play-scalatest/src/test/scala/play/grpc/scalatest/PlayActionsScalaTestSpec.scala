702cf707458e50a9cb2b144974ede36be26be3ea
drZoid <62855774+drZoid@users.noreply.github.com>
2020-09-03 00:10:42 -0700

    fix router using actions template and add tests (#296)
    * fix router using actions template and add tests
    
    * fix headers and formatting

---
+++ play-scalatest/src/test/scala/play/grpc/scalatest/PlayActionsScalaTestSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.grpc.scalatest
+
+import org.scalatest.concurrent.IntegrationPatience
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerTest
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.ws.WSClient
+import play.api.routing.Router
+import io.grpc.Status
+import example.myapp.helloworld.grpc.actions.helloworld._
+
+import akka.grpc.internal.GrpcProtocolNative
+
+/**
+ * Test for the Play gRPC ScalaTest APIs
+ */
+class PlayActionsScalaTestSpec
+    extends PlaySpec
+    with GuiceOneServerPerTest
+    with ServerGrpcClient
+    with ScalaFutures
+    with IntegrationPatience {
+
+  override def fakeApplication(): Application = {
+    GuiceApplicationBuilder()
+      .overrides(bind[Router].to[GreeterServiceWithActionsImpl])
+      .build()
+  }
+
+  implicit def ws: WSClient = app.injector.instanceOf(classOf[WSClient])
+
+  "A Play server bound to a gRPC router using actions" must {
+    "give a 404 when routing a non-gRPC request" in {
+      val result = wsUrl("/").get.futureValue
+      result.status must be(404) // Maybe should be a 426, see #396
+    }
+    // this test results in a 500
+    // "give a 415 error when not using a gRPC content-type" in {
+    //   val result = wsUrl(s"/${GreeterService.name}/FooBar").get.futureValue
+    //   result.status must be(415)
+    // }
+    // this test results in a 500 error
+    // "give a grpc 'unimplemented' error when routing a non-existent gRPC method" in {
+    //   val result = wsUrl(s"/${GreeterService.name}/FooBar")
+    //     .addHttpHeaders("Content-Type" -> GrpcProtocolNative.contentType.toString)
+    //     .get
+    //     .futureValue
+    //   result.status must be(200) // Maybe should be a 426, see #396
+    //   result.header("grpc-status") mustEqual Some(Status.Code.UNIMPLEMENTED.value().toString)
+    // }
+    "give a grpc 'invalid argument' error when routing an empty request to a gRPC method" in {
+      val result = wsUrl(s"/${GreeterService.name}/SayHello")
+        .addHttpHeaders("Content-Type" -> GrpcProtocolNative.contentType.toString)
+        .get
+        .futureValue
+      result.status must be(200)
+      result.header("grpc-status") mustEqual Some(Status.Code.INVALID_ARGUMENT.value().toString)
+    }
+    "work with a gRPC client" in withGrpcClient[GreeterServiceClient] { client: GreeterServiceClient =>
+      val reply = client.sayHello(HelloRequest("Alice")).futureValue
+      reply.message must be("Hello, Alice!")
+    }
+  }
+}
---
+++ play-testkit/src/test/scala/example/myapp/helloworld/grpc/actions/helloworld/GreeterServiceWithActionsImpl.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) Lightbend Inc. <https://www.lightbend.com>
+ */
+package example.myapp.helloworld.grpc.actions.helloworld
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import akka.stream.Materializer
+import akka.actor.ActorSystem
+import play.api.mvc.PlayBodyParsers
+import play.api.mvc.DefaultActionBuilder
+
+import scala.concurrent.Future
+
+@Singleton
+class GreeterServiceWithActionsImpl @Inject() (
+    implicit
+    mat: Materializer,
+    actorSystem: ActorSystem,
+    parsers: PlayBodyParsers,
+    actionBuilder: DefaultActionBuilder,
+) extends AbstractGreeterServiceRouter(
+      mat,
+      actorSystem,
+      parsers,
+      actionBuilder,
+    ) {
+
+  override def sayHello(in: HelloRequest): Future[HelloReply] = {
+    actorSystem.log.error("Saying hello!")
+    Future.successful(HelloReply(s"Hello, ${in.name}!"))
+  }
+
+}
