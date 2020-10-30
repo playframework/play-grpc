62df5b2dbc2d0db1cf8fc9df1eeb6ae4d6fed679
Scala Steward <me@scala-steward.org>
2020-10-30 04:37:34 +0100

    junit 4.13.1 (was 4.13)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
     val akkaActorTestkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % Versions.akka
     val akkaStreamTestkit     = "com.typesafe.akka" %% "akka-stream-testkit"      % Versions.akka
 
-    val junit             = "junit"                   % "junit" % "4.13" % Test // Common Public License 1.0
+    val junit             = "junit"                   % "junit" % "4.13.1" % Test // Common Public License 1.0
     val playAhcWs         = "com.typesafe.play"       %% "play-ahc-ws" % Versions.play % Test // Apache V2
     val playSpecs2        = Compile.playSpecs2        % Test
     val scalaTest         = "org.scalatest"           %% "scalatest" % Versions.scalaTest % Test // Apache V2
