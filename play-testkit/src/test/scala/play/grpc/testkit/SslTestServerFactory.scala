3bb532e54b972c5111efc25d08a413f7aedee7ca
Alfredo Torre <atorre@descartes.com>
2023-12-15 17:34:54 +0100

    Reformatted

--- play-testkit/src/test/scala/play/grpc/testkit/SslTestServerFactory.scala
+++ play-testkit/src/test/scala/play/grpc/testkit/SslTestServerFactory.scala
@@ -16,8 +16,8 @@ import play.core.server.ServerConfig
     super
       .serverConfig(app)
       .copy(
-        port = None,       // disables http port (or use Some(0) for a random port or for a fixed port e.g. 9001 Some(9001))
-        sslPort = Some(0), // Using 0 a random port from the operating system will be assigned
+        port = None,
+        sslPort = Some(0),
       )
   }
 }
