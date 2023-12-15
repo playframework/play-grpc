999ca49f86292cd869f5caacc397a81cce2995a6
Alfredo Torre <atorre@descartes.com>
2023-12-15 17:11:00 +0100

    Add SslTestServerFactory for Test RunningServer

--- play-testkit/src/test/scala/play/grpc/testkit/SslTestServerFactory.scala
+++ play-testkit/src/test/scala/play/grpc/testkit/SslTestServerFactory.scala
@@ -1,11 +1,13 @@
 /*
  * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package play.grpc.testkit
 
 import akka.annotation.ApiMayChange
-import play.api.test.DefaultTestServerFactory
+
 import play.api.Application
+import play.api.test.DefaultTestServerFactory
 import play.core.server.ServerConfig
 
 /**
@@ -16,8 +18,8 @@ import play.core.server.ServerConfig
     super
       .serverConfig(app)
       .copy(
-        port = None,
-        sslPort = Some(0),
+        port = None,      // disables http port (or use Some(0) for a random port or for a fixed port e.g. 9001 Some(9001))
+        sslPort = Some(0),// Using 0 a random port from the operating system will be assigned
       )
   }
 }
