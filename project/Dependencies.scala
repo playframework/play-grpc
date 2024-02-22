b65090f9303bec6240247d1ae93791b6f3f080f5
Matthias Kurz <m.kurz@irregular.at>
2024-02-22 23:32:51 +0100

    Depend on (provided!) BSL libraries that provide Scala 3 artifacts
    This is needed so we can build our Scala 3 artifacts

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,9 +14,13 @@ object Dependencies {
 
     // Don't use AkkaGrpcBuildInfo.akkaHttpVersion or AkkaGrpcBuildInfo.akkaVersion and prioritize
     // aligning with versions transitively brought in via Play.
-    val akka = "2.6.21"
+    def akka(scalaVersion: String) = if (CrossVersion.binaryScalaVersion(scalaVersion) == "3") "2.7.0" else "2.6.21"
     // bumps Akka HTTP version beyond play's 10.1.x
-    val akkaHttp = "10.2.10"
+    def akkaHttp(scalaVersion: String) =
+      if (CrossVersion.binaryScalaVersion(scalaVersion) == "3") "10.5.0" else "10.2.10"
+
+    def conf(scalaVersion: String) =
+      if (CrossVersion.binaryScalaVersion(scalaVersion) == "3") Some(Provided.name) else None
 
     val akkaGrpc: String = AkkaGrpcBuildInfo.version
     val grpc: String     = AkkaGrpcBuildInfo.grpcVersion
@@ -27,35 +31,75 @@ object Dependencies {
     val scalaTestPlusPlay = "6.0.1"
 
     val macwire = "2.5.9"
+
+    def onlyForScala3[T](scalaVersion: String, scala3Only: T): Seq[T] =
+      if (CrossVersion.binaryScalaVersion(scalaVersion) == "3") Seq(scala3Only) else Seq.empty
   }
 
   object Compile {
+    import Versions._
+
     val grpcStub = "io.grpc" % "grpc-stub" % Versions.grpc
 
-    val akkaActorTyped           = "com.typesafe.akka" %% "akka-actor-typed"            % Versions.akka
-    val akkaClusterShardingTyped = "com.typesafe.akka" %% "akka-cluster-sharding-typed" % Versions.akka
-    val akkaStream               = "com.typesafe.akka" %% "akka-stream"                 % Versions.akka
-    val akkaDiscovery            = "com.typesafe.akka" %% "akka-discovery"              % Versions.akka
-    val akkaPersistenceTyped     = "com.typesafe.akka" %% "akka-persistence-typed"      % Versions.akka
-    val akkaPersistenceQuery     = "com.typesafe.akka" %% "akka-persistence-query"      % Versions.akka
-    val akkaSerializationJackson = "com.typesafe.akka" %% "akka-serialization-jackson"  % Versions.akka
+    def akkaActorTyped(sv: String) =
+      ("com.typesafe.akka" %% "akka-actor-typed" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
+    def akkaClusterShardingTyped(sv: String) =
+      ("com.typesafe.akka" %% "akka-cluster-sharding-typed" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
+    def akkaStream(sv: String) =
+      ("com.typesafe.akka" %% "akka-stream" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
+    def akkaDiscovery(sv: String) =
+      ("com.typesafe.akka" %% "akka-discovery" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
+    def akkaPersistenceTyped(sv: String) =
+      ("com.typesafe.akka" %% "akka-persistence-typed" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
+    def akkaPersistenceQuery(sv: String) =
+      ("com.typesafe.akka" %% "akka-persistence-query" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
+    def akkaSerializationJackson(sv: String) =
+      ("com.typesafe.akka" %% "akka-serialization-jackson" % Versions.akka(sv)).withConfigurations(Versions.conf(sv))
 
-    val akkaHttp          = "com.typesafe.akka" %% "akka-http"            % Versions.akkaHttp
-    val akkaHttpSprayJson = "com.typesafe.akka" %% "akka-http-spray-json" % Versions.akkaHttp
-    val akkaHttp2Support  = "com.typesafe.akka" %% "akka-http2-support"   % Versions.akkaHttp
+    def akkaHttp(sv: String) =
+      ("com.typesafe.akka" %% "akka-http" % Versions.akkaHttp(sv)).withConfigurations(Versions.conf(sv))
+    def akkaHttpSprayJson(sv: String) =
+      ("com.typesafe.akka" %% "akka-http-spray-json" % Versions.akkaHttp(sv)).withConfigurations(Versions.conf(sv))
+    def akkaHttp2Support(sv: String) =
+      ("com.typesafe.akka" %% "akka-http2-support" % Versions.akkaHttp(sv)).withConfigurations(Versions.conf(sv))
 
-    val akkaGrpcCodegen = "com.lightbend.akka.grpc" %% "akka-grpc-codegen" % Versions.akkaGrpc // Apache V2
-    val akkaGrpcRuntime = "com.lightbend.akka.grpc" %% "akka-grpc-runtime" % Versions.akkaGrpc // Apache V2
+    def akkaGrpcCodegen(sv: String) = ("com.lightbend.akka.grpc" %% "akka-grpc-codegen" % Versions.akkaGrpc)
+      .withConfigurations(Versions.conf(sv)) // Apache V2 or BSL (depends on USE_BSL sys property)
+    def akkaGrpcRuntime(sv: String) = ("com.lightbend.akka.grpc" %% "akka-grpc-runtime" % Versions.akkaGrpc)
+      .withConfigurations(Versions.conf(sv)) // Apache V2 or BSL (depends on USE_BSL sys property)
 
     val play = ("com.typesafe.play" %% "play" % Versions.play)
       .exclude("javax.activation", "javax.activation-api") // Apache V2 (exclusion is "either GPL or CDDL")
 
-    val playJava             = "com.typesafe.play" %% "play-java"               % Versions.play // Apache V2
-    val playGuice            = "com.typesafe.play" %% "play-guice"              % Versions.play // Apache V2
-    val playAkkaHttpServer   = "com.typesafe.play" %% "play-akka-http-server"   % Versions.play // Apache V2
-    val playAkkaHttp2Support = "com.typesafe.play" %% "play-akka-http2-support" % Versions.play // Apache V2
-    val playTest             = "com.typesafe.play" %% "play-test"               % Versions.play // Apache V2
-    val playSpecs2           = "com.typesafe.play" %% "play-specs2"             % Versions.play // Apache V2
+    val playJava  = "com.typesafe.play" %% "play-java"  % Versions.play // Apache V2
+    val playGuice = "com.typesafe.play" %% "play-guice" % Versions.play // Apache V2
+    def playAkkaHttpServer(sv: String) = Seq(
+      ("com.typesafe.play" %% "play-akka-http-server" % Versions.play) // Apache V2
+        .excludeAll(onlyForScala3(sv, ExclusionRule("com.typesafe.akka", "akka-http-core_2.13")): _*)
+    ) ++
+      onlyForScala3(
+        sv,
+        "com.typesafe.akka" %% "akka-http-core" % Versions.akkaHttp(
+          sv
+        ) % Provided // Apache V2 when Scala 2 or BSL when Scala 3
+      )
+    def playAkkaHttp2Support(sv: String) = Seq(
+      ("com.typesafe.play" %% "play-akka-http2-support" % Versions.play) // Apache V2 when Scala 2 or BSL when Scala 3
+        .excludeAll(onlyForScala3(sv, ExclusionRule("com.typesafe.akka", "akka-http-core_2.13")): _*)
+        .excludeAll(onlyForScala3(sv, ExclusionRule("com.typesafe.akka", "akka-http2-support_2.13")): _*)
+    ) ++
+      onlyForScala3(
+        sv,
+        "com.typesafe.akka" %% "akka-http-core" % Versions.akkaHttp(
+          sv
+        ) % Provided // Apache V2 when Scala 2 or BSL when Scala 3
+      ) ++
+      onlyForScala3(
+        sv,
+        "com.typesafe.akka" %% "akka-http2-support" % Versions.akkaHttp(sv) % Provided
+      ) // Apache V2 when Scala 2 or BSL when Scala 3
+    val playTest   = "com.typesafe.play" %% "play-test"   % Versions.play // Apache V2
+    val playSpecs2 = "com.typesafe.play" %% "play-specs2" % Versions.play // Apache V2
 
     val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % Versions.scalaTestPlusPlay // Apache V2
 
@@ -65,8 +109,10 @@ object Dependencies {
   object Test {
     final val Test = sbt.Test
 
-    val akkaActorTestkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % Versions.akka
-    val akkaStreamTestkit     = "com.typesafe.akka" %% "akka-stream-testkit"      % Versions.akka
+    def akkaActorTestkitTyped(sv: String) = ("com.typesafe.akka" %% "akka-actor-testkit-typed" % Versions.akka(sv))
+      .withConfigurations(Versions.conf(sv)) // Apache V2 when Scala 2 or BSL when Scala 3
+    def akkaStreamTestkit(sv: String) = ("com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka(sv))
+      .withConfigurations(Versions.conf(sv)) // Apache V2 when Scala 2 or BSL when Scala 3
 
     val junit             = "junit"                   % "junit"       % "4.13.2"           % Test // Common Public License 1.0
     val playAhcWs         = "com.typesafe.play"      %% "play-ahc-ws" % Versions.play      % Test // Apache V2
