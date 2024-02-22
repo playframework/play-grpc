762c63991d3825d20fce3668c9f2f794abed5ffd
Matthias Kurz <m.kurz@irregular.at>
2024-02-22 23:32:51 +0100

    Depend on BSL libraries that provide Scala 3 artifacts

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,9 +14,9 @@ object Dependencies {
 
     // Don't use AkkaGrpcBuildInfo.akkaHttpVersion or AkkaGrpcBuildInfo.akkaVersion and prioritize
     // aligning with versions transitively brought in via Play.
-    val akka = "2.6.21"
+    val akka = "2.7.0" // TODO: BSL! Do not publish!
     // bumps Akka HTTP version beyond play's 10.1.x
-    val akkaHttp = "10.2.10"
+    val akkaHttp = "10.5.0" // TODO: BSL! Do not publish!
 
     val akkaGrpc: String = AkkaGrpcBuildInfo.version
     val grpc: String     = AkkaGrpcBuildInfo.grpcVersion
@@ -32,30 +32,33 @@ object Dependencies {
   object Compile {
     val grpcStub = "io.grpc" % "grpc-stub" % Versions.grpc
 
-    val akkaActorTyped           = "com.typesafe.akka" %% "akka-actor-typed"            % Versions.akka
-    val akkaClusterShardingTyped = "com.typesafe.akka" %% "akka-cluster-sharding-typed" % Versions.akka
-    val akkaStream               = "com.typesafe.akka" %% "akka-stream"                 % Versions.akka
-    val akkaDiscovery            = "com.typesafe.akka" %% "akka-discovery"              % Versions.akka
-    val akkaPersistenceTyped     = "com.typesafe.akka" %% "akka-persistence-typed"      % Versions.akka
-    val akkaPersistenceQuery     = "com.typesafe.akka" %% "akka-persistence-query"      % Versions.akka
-    val akkaSerializationJackson = "com.typesafe.akka" %% "akka-serialization-jackson"  % Versions.akka
+    val akkaActorTyped           = "com.typesafe.akka" %% "akka-actor-typed"            % Versions.akka % Provided
+    val akkaClusterShardingTyped = "com.typesafe.akka" %% "akka-cluster-sharding-typed" % Versions.akka % Provided
+    val akkaStream               = "com.typesafe.akka" %% "akka-stream"                 % Versions.akka % Provided
+    val akkaDiscovery            = "com.typesafe.akka" %% "akka-discovery"              % Versions.akka % Provided
+    val akkaPersistenceTyped     = "com.typesafe.akka" %% "akka-persistence-typed"      % Versions.akka % Provided
+    val akkaPersistenceQuery     = "com.typesafe.akka" %% "akka-persistence-query"      % Versions.akka % Provided
+    val akkaSerializationJackson = "com.typesafe.akka" %% "akka-serialization-jackson"  % Versions.akka % Provided
 
-    val akkaHttp          = "com.typesafe.akka" %% "akka-http"            % Versions.akkaHttp
-    val akkaHttpSprayJson = "com.typesafe.akka" %% "akka-http-spray-json" % Versions.akkaHttp
-    val akkaHttp2Support  = "com.typesafe.akka" %% "akka-http2-support"   % Versions.akkaHttp
+    val akkaHttp          = "com.typesafe.akka" %% "akka-http"            % Versions.akkaHttp % Provided
+    val akkaHttpSprayJson = "com.typesafe.akka" %% "akka-http-spray-json" % Versions.akkaHttp % Provided
+    val akkaHttp2Support  = "com.typesafe.akka" %% "akka-http2-support"   % Versions.akkaHttp % Provided
 
-    val akkaGrpcCodegen = "com.lightbend.akka.grpc" %% "akka-grpc-codegen" % Versions.akkaGrpc // Apache V2
-    val akkaGrpcRuntime = "com.lightbend.akka.grpc" %% "akka-grpc-runtime" % Versions.akkaGrpc // Apache V2
+    val akkaGrpcCodegen = "com.lightbend.akka.grpc" %% "akka-grpc-codegen" % Versions.akkaGrpc % Provided // Apache V2
+    val akkaGrpcRuntime = "com.lightbend.akka.grpc" %% "akka-grpc-runtime" % Versions.akkaGrpc % Provided // Apache V2
 
     val play = ("com.typesafe.play" %% "play" % Versions.play)
       .exclude("javax.activation", "javax.activation-api") // Apache V2 (exclusion is "either GPL or CDDL")
 
-    val playJava             = "com.typesafe.play" %% "play-java"               % Versions.play // Apache V2
-    val playGuice            = "com.typesafe.play" %% "play-guice"              % Versions.play // Apache V2
-    val playAkkaHttpServer   = "com.typesafe.play" %% "play-akka-http-server"   % Versions.play // Apache V2
-    val playAkkaHttp2Support = "com.typesafe.play" %% "play-akka-http2-support" % Versions.play // Apache V2
-    val playTest             = "com.typesafe.play" %% "play-test"               % Versions.play // Apache V2
-    val playSpecs2           = "com.typesafe.play" %% "play-specs2"             % Versions.play // Apache V2
+    val playJava  = "com.typesafe.play" %% "play-java"  % Versions.play // Apache V2
+    val playGuice = "com.typesafe.play" %% "play-guice" % Versions.play // Apache V2
+    val playAkkaHttpServer = ("com.typesafe.play" %% "play-akka-http-server" % Versions.play) // Apache V2
+      .excludeAll(ExclusionRule("com.typesafe.akka", "akka-http-core_2.13"))
+    val playAkkaHttp2Support = ("com.typesafe.play" %% "play-akka-http2-support" % Versions.play) // Apache V2
+      .excludeAll(ExclusionRule("com.typesafe.akka", "akka-http-core_2.13"))
+      .excludeAll(ExclusionRule("com.typesafe.akka", "akka-http2-support_2.13"))
+    val playTest   = "com.typesafe.play" %% "play-test"   % Versions.play // Apache V2
+    val playSpecs2 = "com.typesafe.play" %% "play-specs2" % Versions.play // Apache V2
 
     val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % Versions.scalaTestPlusPlay // Apache V2
 
@@ -65,8 +68,8 @@ object Dependencies {
   object Test {
     final val Test = sbt.Test
 
-    val akkaActorTestkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % Versions.akka
-    val akkaStreamTestkit     = "com.typesafe.akka" %% "akka-stream-testkit"      % Versions.akka
+    val akkaActorTestkitTyped = "com.typesafe.akka" %% "akka-actor-testkit-typed" % Versions.akka % Provided
+    val akkaStreamTestkit     = "com.typesafe.akka" %% "akka-stream-testkit"      % Versions.akka % Provided
 
     val junit             = "junit"                   % "junit"       % "4.13.2"           % Test // Common Public License 1.0
     val playAhcWs         = "com.typesafe.play"      %% "play-ahc-ws" % Versions.play      % Test // Apache V2
