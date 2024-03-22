b32bac894df337f2d187c161f56a4585ffa22f6e
Scala Steward <scala_steward@virtuslab.com>
2024-03-22 20:28:02 +0000

    scala-library 2.12.19 (was 2.12.18)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ import org.apache.pekko.grpc.gen.{ BuildInfo => PekkoGrpcBuildInfo }
 object Dependencies {
 
   object Versions {
-    val scala212 = "2.12.18"
+    val scala212 = "2.12.19"
     val scala213 = "2.13.12"
     val scala3   = "3.3.1"
 
