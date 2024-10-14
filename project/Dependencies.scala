f3ef8f7384596b65acbc2b8eb6645afabb09bd2a
Scala Steward <scala_steward@virtuslab.com>
2024-10-14 19:26:52 +0000

    scala-library 2.13.15 (was 2.13.14)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
 
   object Versions {
     val scala212 = "2.12.20"
-    val scala213 = "2.13.14"
+    val scala213 = "2.13.15"
     val scala3   = "3.3.3"
 
     // Don't use PekkoGrpcBuildInfo.pekkoHttpVersion or PekkoGrpcBuildInfo.pekkoVersion and prioritize
