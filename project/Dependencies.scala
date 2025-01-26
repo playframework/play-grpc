cd7fca91e29427d8bb3daf3636744a94e1dbe846
Matthias Kurz <m.kurz@irregular.at>
2025-01-26 22:43:27 +0100

    Play milestone that uses jakarta inject now

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val pekkoGrpc: String = PekkoGrpcBuildInfo.version
     val grpc: String      = PekkoGrpcBuildInfo.grpcVersion
 
-    val play = "3.0.6"
+    val play = "3.1.0-M1"
 
     val scalaTest         = "3.2.17"
     val scalaTestPlusPlay = "7.0.1"
